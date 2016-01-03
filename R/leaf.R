92d4ea0672374852f73f5349af6201d7140b48ad
knausb <briank.lists@gmail.com>
2016-01-03 15:35:00 -0800

    created leaf.R

---
+++ R/leaf.R
@@ -0,0 +1,22 @@
+
+
+#' @rdname leaf
+#' @title leaf
+#'
+#' @description Plot a leaf
+#' 
+#' 
+#' @details 
+#' Plot attitude of structures
+#' 
+#' 
+#' @seealso 
+#' aplpack::faces
+#' 
+#' 
+leaf <- function(){
+  
+} 
+
+
+
