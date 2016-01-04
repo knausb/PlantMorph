91166bd4c77ed6ae21f6335285d360e4a2590f30
knausb <briank.lists@gmail.com>
2016-01-03 19:14:45 -0800

    updated leaf.R

--- R/leaf.R
+++ R/leaf.R
@@ -12,11 +12,21 @@
 #' 
 #' @seealso 
 #' aplpack::faces
+#' stats::density
+#' graphics::polygon
 #' 
 #' 
 leaf <- function(){
+  leaf <- matrix(ncol=2, nrow=10)
+  colnames(leaf) <- c("x", "y")
+  leaf[,'y'] <- c(-2:2,2:-2)
   
-} 
+  leaf[,'x'] <- dnorm( x=leaf[,'y'], mean = 0, sd = 1 )
+  leaf[6:10,'x'] <- -1 *   leaf[6:10,'x']
+  
+  plot( leaf[,"x"], leaf[,"y"], type="n", xlim=c(-1,1), ylim=c(-2,2))
+  polygon( x = leaf[,"x"], y = leaf[,"y"], col="forestgreen")
+}
 
 
 
