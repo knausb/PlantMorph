9db7fdc4ce08470aefc40f68c8f9a53d28ecf486
knausb <briank.lists@gmail.com>
2016-05-28 18:53:56 -0700

    updated character_report2.R

--- R/character_report2.R
+++ R/character_report2.R
@@ -14,12 +14,15 @@
 #' 
 #' 
 #' @examples 
-#' myData <- data.frame(matrix(1:8, ncol=4, nrow=8))
+#' myData <- data.frame(matrix(ncol=4, nrow=12))
 #' colnames(myData) <- paste('char', 1:ncol(myData), sep="_")
 #' rownames(myData) <- paste('sample', 1:nrow(myData), sep="_")
-#' myData[,2] <- as.character(myData[,2])
-#' myData[,3] <- as.factor(myData[,3])
-#' myData[,4] <- factor(myData[,4], ordered = TRUE)
+#' set.seed(9)
+#' myData[,1] <- abs(round(rnorm(nrow(myData), mean = 5, sd =2)))
+#' myData[,2] <- letters[myData[,1]]
+#' myData[,3] <- as.factor(myData[,2])
+#' myData[,4] <- factor(myData[,2], ordered = TRUE)
+#' myData[3,2] <- "?" # Add an nexpected value
 #' 
 #' \dontrun{
 #' character_report2(myData)
@@ -28,7 +31,9 @@
 #' @export
 #' 
 character_report2 <- function(x){
+  
   save(x, file="character_report.RData")
+  
   write('---', file='character_report.Rmd')
   write('title: "Character Report"', file='character_report.Rmd', append = TRUE)
   write('author: "Brian J. Knaus"', file='character_report.Rmd', append = TRUE)
@@ -38,38 +43,46 @@ character_report2 <- function(x){
   write('    toc: true', file='character_report.Rmd', append = TRUE)
   write('---', file='character_report.Rmd', append = TRUE)
   write('', file='character_report.Rmd', append = TRUE)
+  
   write('```{r setup, include=FALSE}', file='character_report.Rmd', append = TRUE)
   write('knitr::opts_chunk$set(fig.align = "center")', file='character_report.Rmd', append = TRUE)
   write('```', file='character_report.Rmd', append = TRUE)
   write('', file='character_report.Rmd', append = TRUE)
+  
   write('```{r load data, echo=FALSE}', file='character_report.Rmd', append = TRUE)
   write('load(file="character_report.RData")',
         file='character_report.Rmd', append = TRUE)
-  #  dump('x', file='character_report.Rmd', append = TRUE)
   write('```', file='character_report.Rmd', append = TRUE)
+
+  # Loop through characters.
   for(i in 1:ncol(x)){
     write('', file='character_report.Rmd', append = TRUE)
     write('*****', file='character_report.Rmd', append = TRUE)
     write('', file='character_report.Rmd', append = TRUE)
-    #    write( paste('## `r Character ', i, ': ', colnames(x)[i], '`', sep=""),
-    #    write( paste('## Character ', i, ' ', colnames(x)[i], '`', sep=""),
+
+    # Character header.
     write( paste('## Character ', i, ' ', colnames(x)[i], sep=""),
            file='character_report.Rmd', append = TRUE)
     write('', file='character_report.Rmd', append = TRUE)
-    #    write('```{r, results="asis"}', file='character_report.Rmd', append = TRUE)
+
+    
     write('```{r, results="asis", echo = FALSE}', file='character_report.Rmd', append = TRUE)
-    write( paste('if( is.ordered( x[ ,colnames(x)[', i, '] ] ) ){', sep=""), file='character_report.Rmd', append = TRUE)
-    #    write( paste('  tmp <- levels(x[ ,colnames(x)[', i, '] ])', sep=""), file='character_report.Rmd', append = TRUE)
+    
+    # Determine class.
+    write( paste('if( is.factor( x[ ,colnames(x)[', i, '] ] ) & is.ordered( x[ ,colnames(x)[', i, '] ] ) ){', sep=""), file='character_report.Rmd', append = TRUE)
     write( paste('  tmp <- as.character( x[ ,colnames(x)[', i, '] ] )', sep=""), file='character_report.Rmd', append = TRUE)
     write( '  tbl.title <- "Ordered factor" ', file='character_report.Rmd', append = TRUE)
+    write( paste('} else if( is.factor( x[ ,colnames(x)[', i, '] ] ) & is.ordered( x[ ,colnames(x)[', i, '] ] ) == FALSE ){', sep=""), file='character_report.Rmd', append = TRUE)
+    write( paste('  tmp <- as.character( x[ ,colnames(x)[', i, '] ] )', sep=""), file='character_report.Rmd', append = TRUE)
+    write( '  tbl.title <- "Factor" ', file='character_report.Rmd', append = TRUE)
     write('} else {', file='character_report.Rmd', append = TRUE)
     write( paste('  tmp <- x[ ,colnames(x)[', i, '] ]', sep=""), file='character_report.Rmd', append = TRUE)
     write('  tmp <- as.character(tmp)', file='character_report.Rmd', append = TRUE)
-    #    write('  tmp <- unique(tmp)', file='character_report.Rmd', append = TRUE)
     write('  tmp <- na.omit(tmp)', file='character_report.Rmd', append = TRUE)
     write( '  tbl.title <- "Character" ', file='character_report.Rmd', append = TRUE)
     write('}', file='character_report.Rmd', append = TRUE)
     write('', file='character_report.Rmd', append = TRUE)
+    
     write('tmp2 <- suppressWarnings( as.numeric(tmp) )', file='character_report.Rmd', append = TRUE)
     write('numbs <- tmp2[ !is.na( tmp2 ) ]', file='character_report.Rmd', append = TRUE)
     write('chars <- tmp[ is.na( tmp2 ) ]', file='character_report.Rmd', append = TRUE)
@@ -93,6 +106,16 @@ character_report2 <- function(x){
     #    write('    message("Only first 10 rows presented")', file='character_report.Rmd', append = TRUE)
     write('  }', file='character_report.Rmd', append = TRUE)
     write('}', file='character_report.Rmd', append = TRUE)
+    write('', file='character_report.Rmd', append = TRUE)
+    
+    # Ordered factor.
+    write( paste('if( is.factor( x[ ,colnames(x)[', i, '] ] ) & is.ordered( x[ ,colnames(x)[', i, '] ] ) ){', sep=""), file='character_report.Rmd', append = TRUE)
+    write( paste('  tmp <- levels(x[ ,colnames(x)[', i, ']])', sep=""), file='character_report.Rmd', append = TRUE)
+    write('  cat("**Factor order: **")', file='character_report.Rmd', append = TRUE)
+    write( '  paste(tmp, collapse = " < ")', file='character_report.Rmd', append = TRUE)
+    write('}', file='character_report.Rmd', append = TRUE)
+    write('', file='character_report.Rmd', append = TRUE)
+    
     write('```', file='character_report.Rmd', append = TRUE)
     write('', file='character_report.Rmd', append = TRUE)
   }
